b4459da23b9c4934bb3c747381f2d478b664a8c4
GraySora16 <qminhqb@gmail.com>
2022-11-12 10:34:28 +0700

    add Jquery

--- update_quantity_in_cart.php
+++ update_quantity_in_cart.php
@@ -2,7 +2,7 @@
     session_start();
     $type = $_GET['type'];
     $id = $_GET['id'];
-    if ($type === 'dec'){
+    if ($type === '0'){
     if ($_SESSION['cart'][$id]['quantity'] > 1){
         $_SESSION['cart'][$id]['quantity']--;
     }else{
--- view_cart.php
+++ view_cart.php
@@ -7,13 +7,16 @@
     <title>Document</title>
 </head>
 <body>
+    <?php include 'header.php' ?>
     <?php 
-    session_start();
     if (!empty($_SESSION['cart'])) {
         $cart = $_SESSION['cart'];
         $sum = 0;
     ?> 
-    <center><h1>Giỏ hàng của bạn</h1></center>
+    <center><h1>
+            Giỏ hàng của bạn
+    </h1></center>
+    <span id="span-view-cart">
     <table border="1" width="100%">
     <tr>
         <th>Ảnh</th>
@@ -28,34 +31,46 @@
             <td>
                 <img src="admin/products/photos/<?php echo $value['image'] ?>" height="100">
                 <td><?php echo $value['name'] ?></td>
-                <td><?php echo $value['price'] ?></td>
                 <td>
-                    <button class="btn-update-quantity" data-id="<?php echo $id ?>" data-type="dec">
+                    <span class="span-price">
+                        <?php echo $value['price'] ?>
+                    </span>
+                </td>
+                <td>
+                    <button class="btn-update-quantity" data-id="<?php echo $id ?>" data-type="0">
                         -
                     </button>
-                    <center><?php echo $value['quantity'] ?></center>
-                    <button class="btn-update-quantity" data-id="<?php echo $id ?>" data-type="inc">
+                    <center>
+                        <span class="span-quantity">
+                            <?php echo $value['quantity'] ?>
+                        </span>
+                    </center>
+                    <button class="btn-update-quantity" data-id="<?php echo $id ?>" data-type="1">
                         +
                     </button>
                 </td>
                 <td>
+                    <span class="span-sum">
                     <?php 
                         $sum+= $value['quantity'] * $value['price'];
                         echo $value['quantity'] * $value['price'] 
                     ?>
+                    </span>
                 </td>
             </td>
             <td>
                 <center>
-                <a href="delete_product_from_cart.php?id=<?php echo $id ?>">
-                    <button>Xóa</button>
-                </a>
+                    <button class="btn-remove-product" data-id="<?php echo $id ?>">Xóa đơn</button>
                 </center>
             </td>
         </tr>     
     <?php }?>
     </table>
-    <h1>Tổng tiền phải trả: <?php echo $sum ?></h1>
+    <h1>Tổng tiền phải trả: 
+        <span id="span-total-price">
+            <?php echo $sum ?>
+        </span>
+    </h1>
     <?php 
         $id = $_SESSION['id'];
         require 'admin/connect.php';
@@ -79,23 +94,61 @@
     <button>Đặt hàng</button>
     </form>
     </center>
+    </span>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
             $(".btn-update-quantity").click(function()  { 
-                let id = $(this).data('id');
-                let type = $(this).data('type');
+                let btn = $(this);
+                let id = btn.data('id');
+                let type = parseInt(btn.data('type'));
                 $.ajax({
                     type: "GET",
                     url: "update_quantity_in_cart.php",
                     data: {id,type},
-                    success: function (response) {
-                        
+                    success: function () {
+                        let tr_parent = btn.parents('tr');
+                        let quantity = parseInt(tr_parent.find(".span-quantity").text());
+                        let price = parseFloat(tr_parent.find(".span-price").text());
+                        quantity = (type == 1 ? quantity+1 : quantity-1)
+                        if (quantity === 0){
+                            tr_parent.remove()
+                        }else{
+                        tr_parent.find('.span-quantity').text(quantity);
+                        let sum = quantity * price
+                        tr_parent.find('.span-sum').text(sum);
+                        }
+                        getTotalPrice();
+
                     }
+                });
+            });
+            $(".btn-remove-product").click(function () { 
+                let btn = $(this)
+                let id = $(this).data('id')
+                $.ajax({
+                    type: "GET",
+                    url: "delete_product_from_cart.php",
+                    data: {id},
+                    success: function () {
+                            tr_parent = btn.parents("tr")
+                            tr_parent.remove()
+                            getTotalPrice();
+                            if (total == 0){
+                                $("#span-view-cart").text("")
+                            }
+                        }
                 });
                 
             });
-        });    
+        }); 
+    function getTotalPrice(){
+        let total = 0;
+        $(".span-sum").each(function () {
+        total += parseFloat($(this).text())
+        });
+        $("#span-total-price").text(total);
+    }   
     </script>
     <?php }else{ ?>
         <center><h1>Giỏ hàng trống</h1></center>
